65c4903874f996e779aad2bd0ab8120113ae2107
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-09-09 12:38:49 +0200

    add auth guard on routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({}),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,1 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+
+    const secret = process.env.JWT_ACCESS_SECRET;
+      if (!secret) {
+        throw new Error('JWT_ACCESS_SECRET must be defined');
+      }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
